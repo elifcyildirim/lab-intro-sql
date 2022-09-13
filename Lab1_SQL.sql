09a448f70b59eba3b961228c88469950af566fda
elif yildirim <elifyildirim@elifs-MacBook-Air-2.local>
2022-09-13 18:33:06 +0300

    completed SQL_intro_lab

---
+++ Lab1_SQL.sql
@@ -0,0 +1,17 @@
+USE sakila;
+SELECT *
+FROM actor;
+SELECT *
+FROM film;
+SELECT *
+FROM customer;
+SELECT title
+FROM film;
+SELECT name
+FROM language;
+SELECT address_id
+FROM store;
+SELECT staff_id
+FROM staff;
+SELECT first_name
+FROM staff;
